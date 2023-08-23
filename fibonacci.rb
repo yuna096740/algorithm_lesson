843f16e3b037f7ea15ae4a8f3e1b2333dbdae198
yuna096740 <yyuusseeii1111@gmail.com>
2023-08-23 20:05:08 +0900

    [add] fibonaci

---
+++ fibonacci.rb
@@ -0,0 +1,15 @@
+$memo = {}
+
+def fibonacci(num)
+  if $memo.has_key?(num)
+    return $memo[num]
+  elsif num == 0
+    return 0
+  elsif num == 1
+    return 1
+  else
+    return $memo[num] = fibonacci(num - 1) + fibonacci(num - 2)
+  end
+end
+
+puts fibonacci(18)
